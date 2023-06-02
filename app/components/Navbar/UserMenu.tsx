302846261653c5f2d49b0e8253193ade28b4d81d
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-02 19:00:14 +0300

    add login/signe up in user menu

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -1,9 +1,16 @@
 'use client';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {AiOutlineMenu} from 'react-icons/ai';
 import Avatar from '../Avatar';
+import MenuItem from './MenuItem';
 
 const UserMenu = () => {
+	const [isOpen, setIsOpen]= useState(false);
+
+	const toggleOpen = useCallback(() => {
+		setIsOpen((value) => !value);
+	},[]);
+
 	return (
 		<div className='relative'>
 			<div className='flex flex-row items-center gap-3'>
@@ -14,7 +21,7 @@ const UserMenu = () => {
 					Your Home
 				</div>
 				<div 
-				onClick={() => {}}
+				onClick={toggleOpen}
 				className='flex flex-row items-center gap-3 rounded-full p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 hover:shadow-md transition cursor-pointer'
 				>
 					<AiOutlineMenu/>
@@ -23,6 +30,23 @@ const UserMenu = () => {
 					</div>
 				</div>
 			</div>
+
+			{isOpen && (
+				<div className='absolute top-12 right-0 rounded-xl shadow-md w-[40vw] md:w-3/4  overflow-hidden bg-white  text-sm'>
+					<div className='flex flex-col cursor-pointer'>
+						<>
+							<MenuItem
+							onClick={() => {}}
+							label='Login'
+							/>
+							<MenuItem
+							onClick={() => {}}
+							label='Sign up'
+							/>
+						</>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
