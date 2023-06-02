dab7230d9424539c81dff8232a17b4c6ac8e8c17
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-02 18:25:11 +0300

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="font-bold text-blue-500 text-2xl">Hellow world</h1>
+      <h1 className='font-bold text-blue-500 text-2xl'>Hellow world</h1>
     </main>
   )
 }
