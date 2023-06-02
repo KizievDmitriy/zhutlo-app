248ec2dc9d28bdd5f53dfcff8c07ea74df7475de
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-02 16:40:49 +0300

    add next-app-template

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Zhutlo',
+  description: 'Apartment search app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-bold text-blue-500 text-2xl">Hellow world</h1>
+    </main>
+  )
+}
