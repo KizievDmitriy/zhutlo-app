91431320290987efe4dc8d3641555628997c2c87
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-03 15:07:03 +0300

    add inputs registerModal

--- app/components/Modals/RegisterModal.tsx
+++ app/components/Modals/RegisterModal.tsx
@@ -8,6 +8,7 @@ import{ FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import Modal from './Modal';
 import Heading from '../Heading';
+import Input from '../Inputs/Input';
 
 const RegisterModal = () => {
 	const registerModal = useRegisterModal();
@@ -48,6 +49,31 @@ const RegisterModal = () => {
 					title='Welcome to Zhutlo'
 					subtitle='Create an acount!'
 					center
+				/>
+				<Input
+					id='email'
+					label='Email'
+					disabled={isLoading}
+					register={register}
+					errors={errors}
+					required
+				/>
+				<Input
+					id='name'
+					label='Name'
+					disabled={isLoading}
+					register={register}
+					errors={errors}
+					required
+				/>
+				<Input
+					id='password'
+					type='password'
+					label='Password'
+					disabled={isLoading}
+					register={register}
+					errors={errors}
+					required
 				/>
 		</div>
 	);
