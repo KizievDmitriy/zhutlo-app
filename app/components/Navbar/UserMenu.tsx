6646f34ea92c034610633970190bdc9d283e8785
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-03 20:46:54 +0300

    add loginModal

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -4,9 +4,11 @@ import {AiOutlineMenu} from 'react-icons/ai';
 import Avatar from '../Avatar';
 import MenuItem from './MenuItem';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 
 const UserMenu = () => {
 	const registerModal = useRegisterModal();
+	const loginModal = useLoginModal();
 	const [isOpen, setIsOpen]= useState(false);
 
 	const toggleOpen = useCallback(() => {
@@ -38,7 +40,7 @@ const UserMenu = () => {
 					<div className='flex flex-col cursor-pointer'>
 						<>
 							<MenuItem
-							onClick={() => {}}
+							onClick={loginModal.onOpen}
 							label='Login'
 							/>
 							<MenuItem
