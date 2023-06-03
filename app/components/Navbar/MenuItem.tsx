96bfcfdb4fb27ca6b5b2f66f0969c582a8f39623
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-03 21:18:31 +0300

    add some style

--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -10,7 +10,7 @@ const MenuItem: React.FC<MenuItemProps> = ({onClick, label}) => {
 	return (
 		<div 
 		onClick={onClick}
-		className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'>
+		className='px-4 py-3 hover:bg-neutral-100 hover:text-blue-500  transition font-semibold'>
 			{label}
 		</div>
 	)
