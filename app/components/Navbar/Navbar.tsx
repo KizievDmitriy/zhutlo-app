b28d20704919e5f764a0d110e09310d17859b067
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-04 19:12:39 +0300

    add categories

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import Logo from './Logo'
 import Search from './Search'
 import UserMenu from './UserMenu'
 import { SafeUser } from '@/app/types';
+import Categories from './Categories';
 
 
 interface NavbarProps {
@@ -24,6 +25,7 @@ interface NavbarProps {
 					</div>
 				</Container>
 			</div>
+			<Categories/>
 		</div>
 	)
 }
