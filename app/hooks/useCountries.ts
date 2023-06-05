08f944e27181484af96124d7b9815581d90416ef
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-05 15:17:37 +0300

    create hook for countries

---
+++ app/hooks/useCountries.ts
@@ -0,0 +1,24 @@
+import countries from 'world-countries';
+
+const formatedCoutries = countries.map((countrie) => ({   //return obj
+	value: countrie.cca2,
+	label: countrie.name.common,
+	flag: countrie.flag,
+	latlng: countrie.latlng,
+	region: countrie.region
+}));
+
+const useCoutries = () => {
+	const getAll = formatedCoutries;
+
+	const getByValue = (value: string) => {
+		return formatedCoutries.find((item) => item.value === value);
+	}
+
+	return {
+		getAll,
+		getByValue
+	}
+}
+
+export default useCoutries
