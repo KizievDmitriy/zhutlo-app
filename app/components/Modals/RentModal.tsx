549728f569e1bac25f686f16ae5fb5d66fa6f729
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-05 20:33:46 +0300

    add submit logic

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 import React, { useMemo, useState } from 'react';
 import Modal from './Modal';
 import useRentModal from '@/app/hooks/useRentModal';
 import Heading from '../Heading';
 import { categories } from '../Navbar/Categories';
 import CategoryInput from '../Inputs/CategoryInput';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import CountrySelect from '../Inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../Inputs/Counter';
 import ImageUpload from '../Inputs/ImageUpload';
 import Input from '../Inputs/Input';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 // import Map from '../Map';  not work normaly!!!
 
 enum STEPS {
@@ -25,6 +28,7 @@ enum STEPS {
 
 
 const RentModal = () => {
+	const router = useRouter();
 	const rentModal = useRentModal();
 	const [step, setStep] = useState(STEPS.CATEGORY);
 	const [isLoading, setIsLoading] = useState(false);
@@ -77,6 +81,28 @@ const RentModal = () => {
 
 	const onNext = () => {
 		setStep((value) => value + 1);
+	}
+
+	const onSubmit: SubmitHandler<FieldValues> = (data) => {
+		if(step !== STEPS.PRICE) {
+			return onNext();
+		}
+
+		setIsLoading(true);
+
+		axios.post('/api/listings', data)
+		.then(() => {
+			toast.success('Listing created!');
+			router.refresh();
+			reset();
+			setStep(STEPS.CATEGORY);
+			rentModal.onClose();
+		})
+		.catch(() => {
+			toast.error('Somthing went wrong');
+		}).finally(() => {
+			setIsLoading(false);
+		})
 	}
 
 	const actionLabel = useMemo(() => {
@@ -239,7 +265,7 @@ const RentModal = () => {
 		<Modal
 			isOpen={rentModal.isOpen}
 			onClose={rentModal.onClose}
-			onSubmit={onNext}
+			onSubmit={handleSubmit(onSubmit)}
 			title='Your home'
 			actionLabel = {actionLabel}
 			secondaryActionLabel = {secondaryActionLabel}
