7bda5193416134b177c11ce89ff8baf160678a0a
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-05 22:41:02 +0300

    fix bug

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -2,7 +2,7 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import prisma from '@/app/libs/prismadb';
 
-export async function getSession() {
+export default async function getSession() {
 	return await getServerSession(authOptions);
 }
 
