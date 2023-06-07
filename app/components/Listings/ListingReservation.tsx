914085b9630d167bddfac61259f4d80560f05a6e
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-07 17:14:37 +0300

    create reserv..

---
+++ app/components/Listings/ListingReservation.tsx
@@ -0,0 +1,22 @@
+'use client';
+import React from 'react';
+import { Range } from 'react-date-range';
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range,
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation: React.FC<ListingReservationProps> = ({price, dateRange, totalPrice, onChangeDate, onSubmit, disabled, disabledDates}) => {
+
+	return (
+		<div>ListingReservation</div>
+	)
+}
+
+export default ListingReservation
