f3226a75374a5d4ba57f65817851c0c4acecab84
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-07 17:13:00 +0300

    add loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import RentModal from './components/Modals/RentModal';
 import Navbar from './components/Navbar/Navbar';
 import ToasterProvider from './providers/ToasterProvider';
 import  getCurrentUser  from './actions/getCurrentUser';
+import { Suspense } from 'react';
+import Loading from './loading';
 
 
 const font = Nunito({ subsets: ['latin'] });
@@ -30,9 +32,11 @@ export default async function RootLayout({
         <RegisterModal/>
         <LoginModal/>
         <Navbar currentUser={currentUser}/>
-        <div className='pb-20 pt-28'>
-          {children}
-        </div>
+        <Suspense fallback={<Loading/>}>
+          <div className='pb-20 pt-28'>
+            {children}
+          </div>
+        </Suspense>
       </body>
     </html>
   )
