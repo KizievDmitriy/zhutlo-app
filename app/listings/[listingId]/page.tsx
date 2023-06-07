4abc6be4b92b84d5bca42d39caed400a0a098b31
KizievDmitriy <kizievdmitrii@gmail.com>
2023-06-07 15:45:31 +0300

    add listing page

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,8 +1,30 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getListingById from '@/app/actions/getListingById';
+import EmptyState from '@/app/components/EmptyState';
 import React from 'react';
+import ListingClient from './ListingClient';
 
-const ListingPage = () => {
+interface IParams {
+  listingId?: string;
+}
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+
+	const listing = await getListingById(params);
+	const currentUser = await getCurrentUser();
+
+	if (!listing) {
+    return (
+      <>
+        <EmptyState />
+      </>
+    );
+  }
 	return (
-		<div>My Listing Page</div>
+		<ListingClient
+			listing={listing}
+			currentUser={currentUser}
+		/>
 	)
 }
 
